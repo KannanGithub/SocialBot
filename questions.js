4dbf2478b4f8f7cf2ed9215112004193fb629f1d
KannanGithub <36538806+KannanGithub@users.noreply.github.com>
2018-02-16 15:45:09 +0000

    Update questions.js
    [KG]Updating questions.js

--- questions.js
+++ questions.js
@@ -1,7 +1,7 @@
  var builder = require('botbuilder');
  const connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
 });
 
 const bot = new builder.UniversalBot(connector, [
